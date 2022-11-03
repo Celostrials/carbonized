416a3847c0f50fa4e53b13ab1a4465a70340b88e
bridgerz <bridgerzoske@gmail.com>
2022-11-03 16:44:33 -0700

    CarbonizedCollection WIP

--- contracts/CarbonizedCollection.sol
+++ contracts/CarbonizedCollection.sol
@@ -9,48 +9,50 @@ import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721Enumer
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-import "./interface/ICarbonizedCollection.sol";
-import "./interface/ICarbonRewards.sol";
+import "./interface/ImpactVaultInterface.sol";
 
 /// @title CarbonizedCollection
 /// @author Bridger Zoske
-/// @notice This contract enables holders of the 'originalCollection' ERC721 to Carbonize and
-/// Decarbonize their NFTs. Carbonizing effectively locks the original NFT and mints the
-/// carbonized version, while Decarbonizing burns the carbonized token and unlocks the original.
+/// @notice
 /// @dev This contract inherits from both ERC721 that ERC721Receiver which enables both mint
 /// and burn as well as the safe storage of other ERC721 tokens.
 contract CarbonizedCollection is
     OwnableUpgradeable,
     ERC721EnumerableUpgradeable,
-    IERC721ReceiverUpgradeable,
-    ICarbonizedCollection
+    IERC721ReceiverUpgradeable
 {
     using SafeERC20Upgradeable for IERC20Upgradeable;
+
+    ImpactVaultInterface gTokenVault;
     IERC721Upgradeable public originalCollection;
-    IERC20Upgradeable public carbonCredit;
-    ICarbonRewards public rewards;
-    uint256 public minCarbon;
-    uint256 public maxCarbon;
+    IERC20Upgradeable public stToken;
+
+    address public carbonCredit;
     string public baseURI;
     string public baseExtension;
+    uint256 public retiredPerGTokenStored;
+
     // tokenId => carbonAmount
     mapping(uint256 => uint256) public carbonDeposit;
-    uint256 public totalCarbon;
+    // tokenId => gTokenBalance
+    mapping(uint256 => uint256) public gTokenBalance;
+    // tokenId => credits retired per token paid
+    mapping(uint256 => uint256) public idRetiredPerTokenPaid;
+    uint256 totalGToken;
+    uint256 carbonCreditsRetired;
 
     function initialize(
         address _originalCollection,
+        address _gTokenVaultAddress,
         address _carbonCredit,
-        address _carbonRewards,
         string memory name,
         string memory symbol
     ) external virtual initializer {
         __Ownable_init();
         __ERC721_init(name, symbol);
         originalCollection = IERC721Upgradeable(_originalCollection);
-        rewards = ICarbonRewards(_carbonRewards);
-        carbonCredit = IERC20Upgradeable(_carbonCredit);
-        minCarbon = 10**IERC20Metadata(_carbonCredit).decimals();
-        maxCarbon = 2 * minCarbon;
+        gTokenVault = ImpactVaultInterface(_gTokenVaultAddress);
+        carbonCredit = _carbonCredit;
         baseExtension = ".json";
         baseURI = "https://ipfs.io/ipfs/QmTn1W5CpTdqrkvdSLb7nXGWVYYmoTWMv8N2ripQthXw2v/";
     }
@@ -59,36 +61,26 @@ contract CarbonizedCollection is
         return baseURI;
     }
 
-    function carbonize(uint256 tokenId, uint256 amount) public {
-        require(carbonDeposit[tokenId] == 0, "CarbonizedCollection: tokenId already carbonized");
-        require(amount >= minCarbon, "CarbonizedCollection: not enough carbon");
-        require(amount <= maxCarbon, "CarbonizedCollection: too much carbon");
-        carbonCredit.safeTransferFrom(msg.sender, address(this), amount);
+    function carbonize(uint256 tokenId, uint256 amount)
+        public
+        _updateCarbonDeposits(int256(tokenId))
+    {
+        gTokenVault.asset().safeTransferFrom(msg.sender, address(this), amount);
+        gTokenVault.asset().approve(address(gTokenVault), amount);
+        gTokenVault.deposit(amount, address(this));
         originalCollection.safeTransferFrom(msg.sender, address(this), tokenId);
-        carbonDeposit[tokenId] = amount;
-        totalCarbon += amount;
+        gTokenBalance[tokenId] += amount;
+        totalGToken += amount;
         mint(tokenId);
-        rewards.updateReward(msg.sender);
     }
 
-    function decarbonize(uint256 tokenId) public {
-        rewards.updateReward(msg.sender);
-        require(carbonDeposit[tokenId] != 0, "CarbonizedCollection: tokenId not carbonized");
-        _burn(tokenId);
+    function decarbonize(uint256 tokenId) public _updateCarbonDeposits(int256(tokenId)) {
+        require(gTokenBalance[tokenId] != 0, "CarbonizedCollection: tokenId has no gToken");
         originalCollection.safeTransferFrom(address(this), msg.sender, tokenId);
-        carbonCredit.safeTransfer(msg.sender, carbonDeposit[tokenId]);
-        totalCarbon -= carbonDeposit[tokenId];
-        carbonDeposit[tokenId] = 0;
-    }
-
-    function _transfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal virtual override {
-        rewards.updateReward(from);
-        rewards.updateReward(to);
-        super._transfer(from, to, tokenId);
+        gTokenVault.withdraw(gTokenBalance[tokenId], msg.sender, msg.sender);
+        totalGToken -= gTokenBalance[tokenId];
+        gTokenBalance[tokenId] = 0;
+        _burn(tokenId);
     }
 
     function carbonizeBatch(uint256[] memory tokenIds, uint256[] memory amounts) external {
@@ -107,19 +99,20 @@ contract CarbonizedCollection is
         }
     }
 
-    function carbonBalance(address account) external view override returns (uint256 carbon) {
-        (, uint256[] memory balances) = walletOfOwner(account);
-        for (uint256 i = 0; i < balances.length; i++) {
-            carbon += balances[i];
+    function retireCarbon(uint256 amount) external _updateCarbonDeposits(-1) {
+        require(
+            IERC20Upgradeable(carbonCredit).balanceOf(address(this)) > 0,
+            "CarbonizedCollection: No credits to retire."
+        );
+        // TODO: retire carbon
+        carbonCreditsRetired += amount;
+    }
+
+    function carbonBalance(address account) external view returns (uint256 carbon) {
+        (, uint256[] memory carbonBalances, ) = walletOfOwner(account);
+        for (uint256 i = 0; i < carbonBalances.length; i++) {
+            carbon += carbonBalances[i];
         }
-    }
-
-    function setMinCarbon(uint256 _minCarbon) external onlyOwner {
-        minCarbon = _minCarbon;
-    }
-
-    function setMaxCarbon(uint256 _maxCarbon) external onlyOwner {
-        maxCarbon = _maxCarbon;
     }
 
     function exists(uint256 tokenId) public view returns (bool) {
@@ -133,16 +126,43 @@ contract CarbonizedCollection is
     function walletOfOwner(address _owner)
         public
         view
-        returns (uint256[] memory, uint256[] memory)
+        returns (
+            uint256[] memory,
+            uint256[] memory,
+            uint256[] memory
+        )
     {
         uint256 ownerTokenCount = balanceOf(_owner);
         uint256[] memory tokenIds = new uint256[](ownerTokenCount);
-        uint256[] memory balances = new uint256[](ownerTokenCount);
+        uint256[] memory carbonDeposits = new uint256[](ownerTokenCount);
+        uint256[] memory gTokenBalances = new uint256[](ownerTokenCount);
         for (uint256 i; i < ownerTokenCount; i++) {
             tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
-            balances[i] = carbonDeposit[tokenIds[i]];
+            carbonDeposits[i] = carbonDeposit[tokenIds[i]];
+            gTokenBalances[i] = gTokenBalance[tokenIds[i]];
         }
-        return (tokenIds, balances);
+        return (tokenIds, carbonDeposits, gTokenBalances);
+    }
+
+    function carbonRetired(uint256 tokenId) public view returns (uint256 carbon) {
+        return (((gTokenBalance[tokenId] * (retiredPerGToken() - idRetiredPerTokenPaid[tokenId])) /
+            1e18) + carbonDeposit[tokenId]);
+    }
+
+    modifier _updateCarbonDeposits(int256 tokenId) {
+        retiredPerGTokenStored = retiredPerGToken();
+        if (tokenId > -1) {
+            carbonDeposit[uint256(tokenId)] = carbonRetired(uint256(tokenId));
+            idRetiredPerTokenPaid[uint256(tokenId)] = retiredPerGTokenStored;
+        }
+        _;
+    }
+
+    function retiredPerGToken() public view returns (uint256) {
+        if (totalGToken == 0) {
+            return retiredPerGTokenStored;
+        }
+        return retiredPerGTokenStored + (carbonCreditsRetired / totalGToken);
     }
 
     function onERC721Received(
---
+++ contracts/interface/ImpactVaultInterface.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: Apache-2.0
+// https://docs.soliditylang.org/en/v0.8.10/style-guide.html
+pragma solidity ^0.8.0;
+
+import {IERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+
+interface ImpactVaultInterface {
+    // Returns the depositing asset.
+    function asset() external returns (IERC20Upgradeable);
+
+    // Returns the yield asset held by the vault.
+    function yieldAsset() external returns (IERC20Upgradeable);
+
+    // Deposit asset on behalf of "_receiver".
+    function deposit(uint256 _amount, address _receiver) external;
+
+    // Withdraws asset to owned by "_owner" to "_receiver".
+    function withdraw(
+        uint256 _amount,
+        address _receiver,
+        address _owner
+    ) external;
+
+    // Withdraws yield asset owned by "_owner" to "_receiver". Note that
+    // "_amount" is denominated in asset so this is converted to the
+    // equivalent in yield asset first.
+    function withdrawYieldAsset(
+        uint256 _amount,
+        address _receiver,
+        address _owner
+    ) external;
+
+    // Returns the total yield earned on vault. Denominated in asset.
+    function totalYield() external view returns (uint256);
+
+    // Returns the total yield earned on vault. Denominated in US dollars.
+    function totalYieldUSD() external view returns (uint256);
+
+    // Returns the total yield earned by "_address" on vault. Denominated in asset.
+    function getYield(address _address) external view returns (uint256);
+
+    // Converts amount of yield asset to amount in asset.
+    function convertToAsset(uint256 _amountYieldAsset) external view returns (uint256);
+
+    // Converts amount of asset to amount in yield asset.
+    function convertToYieldAsset(uint256 _amountAsset) external view returns (uint256);
+}
